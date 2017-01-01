5ef330385ea9f6ebaa066a502689206e56d681ef
Gurpreet Singh <gurpreetsk@hotmail.com>
2017-01-01 23:34:11 +0530

    style: setup splash screen pager

---
+++ app/src/main/java/gurpreetsk/me/motivationdaily/fragments/intro/Fragment4.java
@@ -0,0 +1,30 @@
+package gurpreetsk.me.motivationdaily.fragments.intro;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import gurpreetsk.me.motivationdaily.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment4 extends Fragment {
+
+
+    public Fragment4() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fragment4, container, false);
+    }
+
+}
